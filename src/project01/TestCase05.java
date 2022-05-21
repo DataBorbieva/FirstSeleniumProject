b3f34a733192f5242d7a5f9927f72a14f72d9124
DataBorbieva <databorbieva@gmail.com>
2022-05-20 20:35:06 -0500

    project

--- src/project01/TestCase05.java
+++ src/project01/TestCase05.java
@@ -23,10 +23,16 @@ public class TestCase05 {
         driver.findElement(By.xpath("(//a[@data-ux='NavLink'])[2]")).click();
         ValidationUtilities.validateURL(driver, "https://comfyelite.com/contact-us");
 
-        WebElement signUpBox = driver.findElement(By.xpath("//label[@data-aid='CONTACT_FORM_EMAIL_OPT_IN']"));
+        WebElement signUpBox = driver.findElement(By.xpath("(//div[@data-ux='Element'])[2]"));
+//        WebElement signUpBox = driver.findElement(By.xpath("//label[@data-aid='CONTACT_FORM_EMAIL_OPT_IN']"));
+
+
+        WebElement signupCheck = driver.findElement(By.xpath("//input[@data-ux='InputCheckbox']"));
+
+
         signUpBox.click();
         Thread.sleep(3000);
-        System.out.println("Checked box validation "+(signUpBox.isSelected()?"PASSED":"FAILED")); //Checked box validation FAILED
+        System.out.println("Checked box validation "+(signupCheck.isSelected()?"PASSED":"FAILED")); //Checked box validation FAILED
         driver.quit();
     }
 }
